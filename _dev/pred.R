9c81810d036e71cb70f4fc2ff29c19eb9efd362e
echuu <chuu3ric@gmail.com>
2018-04-08 16:08:42 -0500

    added predict() function, incomplete

--- _dev/pred.R
+++ _dev/pred.R
@@ -1,32 +1,20 @@
 
 
-# response: count
-# count features: child, camper
-# zero features:  persons
-m1 = zeroinfl(count ~ child + camper | persons, data = zinb)
+type_var = 'count'
+preds0 = data.frame(yhat = predict(m1, type = type_var))
+preds1 = data.frame(yhat = zip_pred(m1, type = type_var));
+head(data.frame(preds0, preds1), 20) # output matches
 
-m1$fitted.values
-m1$model
+d = DebTrivedi;
+m = zeroinfl(ofp ~ hosp + health + numchron + gender + 
+                school + privins | health, data = d)
 
-
-preds0 = data.frame(yhat = predict(m1, type = 'response'))
-head(preds, 20)
-
-preds1 = data.frame(yhat = zip_pred(m1, type = 'response'));
-head(preds1, 20)
-
-head(data.frame(preds, preds1), 20) # output matches
-
-# count training matrix with intercept in first column
-testX = model.matrix(m1$terms$count, m1$model, contrasts = m1$contrasts$count)
-# zero training matrix with intercept in first column
-testZ = model.matrix(m1$terms$zero,  m1$model, contrasts = m1$contrasts$zero)  
-
-head(testX)
-head(testZ)
-
-m1$count_X = testX
-m1$zero_X  = testZ
+m$count_X = model.matrix(m0$terms$count, m0$model, contrasts = m0$contrasts$count);
+m$zero_X = model.matrix(m0$terms$zero, m0$model, contrasts = m0$contrasts$zero);
+type_var = 'response'
+preds0 = data.frame(yhat0 = predict(m0, type = type_var))
+preds1 = data.frame(yhat1 = zip_pred(m0, type = type_var))
+head(data.frame(preds0, preds1))
 
 # zip: zeroinfl() model object
 zip_pred = function(m = zip, newdata, type) {
@@ -35,29 +23,26 @@ zip_pred = function(m = zip, newdata, type) {
     rval = m$fitted.values;
     
     if (type != 'response') {
-      
-      rval = m$fitted.values;
-      
-      X = m$count_X;
-      Z = m$count_Z;
-      
+      X    = m$count_X;
+      Z    = m$zero_X;
       # determine offset
       if (is.null(m$offset$count)) {
-        offsetx = rep(0, nrow(m$count_X));
+        offsetx = rep(0, nrow(X));
       } else {
         offsetx = m$offset$count
       }
       
       if (is.null(m$offset$zero)) {
-        offsetz = rep(0, nrow(m$zero_X));
+        offsetz = rep(0, nrow(Z));
       } else {
         offsetz = m$offset$zero;
       }
       # finished determining offset
       
-      mu  = exp(X %*% m$coefficients$count + offsetx)[, 1];
-      phi = m$linkinv(Z %*% m$coefficients$zero + offsetz)[, 1]
+      mu  = exp(X %*% m$coefficients$count + offsetx)[1];
+      phi = m$linkinv(Z %*% m$coefficients$zero + offsetz)[1]
     } # no action if type == 'response' 
+    
   } else { # corresponds to outer if
     print("non-missing newdata option not yet supported");
   } # end of outer if/else
@@ -70,9 +55,35 @@ zip_pred = function(m = zip, newdata, type) {
     rval = phi;
   }
   
-  # if (type == 'prob')
-  
+  if (type == 'prob') {
+    
+    if (!is.null(m$y)) {
+      y = m$y; 
+    } else {
+      print("Predicted Probabilities requires non-null values for y");
+    }
+    
+    yUnique = 0:max(y);
+    nUnique = length(yUnique);
+    rval = matrix(NA, nrow = length(rval), ncol = nUnique);
+    dimnames(rval) = list(rownames(X), yUnique);
+    
+    switch(m$dist,
+           "poisson" = {
+             rval[, 1] = phi + (1-phi) * exp(-mu)
+             for(i in 2:nUnique) rval[,i] = (1-phi) * dpois(yUnique[i], lambda = mu)
+           },
+           "negbin" = {
+             theta = object$theta
+             rval[, 1] = phi + (1-phi) * dnbinom(0, mu = mu, size = theta)
+             for(i in 2:nUnique) rval[,i] = (1-phi) * dnbinom(yUnique[i], mu = mu, size = theta)
+           },
+           "geometric" = {
+             rval[, 1] = phi + (1-phi) * dnbinom(0, mu = mu, size = 1)
+             for(i in 2:nUnique) rval[,i] = (1-phi) * dnbinom(yUnique[i], mu = mu, size = 1)
+           })
+  } # end if() -- 'prob' case
   
   return(rval)
-}
+} # end of zip_pred() function
 
--- _dev/testingOutput.R
+++ _dev/testingOutput.R
@@ -2,7 +2,6 @@
 
 
 library(pscl)
-
 zinb <- read.csv("https://stats.idre.ucla.edu/stat/data/fish.csv")
 zinb <- within(zinb, {
   nofish <- factor(nofish)
@@ -12,9 +11,34 @@ zinb <- within(zinb, {
 
 
 m1 = zeroinfl(count ~ child + camper | persons, data = zinb)
-
 print(m1)
-
 summary(m1)
-
 vcov(m1)
+
+preds = data.frame(yhat = predict(m1, type = 'response'))
+head(preds)
+
+preds1 = data.frame(yhat = predict(m1, type = 'zero'))
+head(preds1)
+
+# response: count
+# count features: child, camper
+# zero features:  persons
+m1 = zeroinfl(count ~ child + camper | persons, data = zinb)
+
+m1$fitted.values
+m1$model
+
+# count training matrix with intercept in first column
+testX = model.matrix(m1$terms$count, m1$model, contrasts = m1$contrasts$count)
+# zero training matrix with intercept in first column
+testZ = model.matrix(m1$terms$zero,  m1$model, contrasts = m1$contrasts$zero)  
+
+m1$count_X = testX
+m1$zero_X  = testZ
+
+
+
+
+
+
